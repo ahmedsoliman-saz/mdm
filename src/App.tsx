6e809e59957171539fdf06e272d001580cd9a487
saz.Heaiba <saz.Heaiba>
2026-04-06 14:52:11 +0200

    --inti app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { MdmHello } from './components/MdmHello'
+
+function App() {
+  return (
+    <main className="app">
+      <MdmHello />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/MdmHello.tsx
@@ -0,0 +1,11 @@
+import './MdmHello.css'
+
+export function MdmHello() {
+  return (
+    <div className="mdm-hello">
+      <p className="mdm-hello__title">MDM</p>
+      <p className="mdm-hello__greeting">hello</p>
+      <p className="mdm-hello__hint">Sample component</p>
+    </div>
+  )
+}
